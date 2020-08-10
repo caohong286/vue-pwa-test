875b5a271dd0e8c004c9ba8d8db647ff38cf8de6
jackcao286 <caohong286@163.com>
2020-08-10 13:12:00 +0800

    deploy

---
+++ precache-manifest.5da3c14e1b8528b6debd9b27a09269ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3fea82f55984ba9ab3a",
+    "url": "/vue-pwa-test/css/app.944ad219.css"
+  },
+  {
+    "revision": "d3fea82f55984ba9ab3a",
+    "url": "/vue-pwa-test/js/app.8bc8788e.js"
+  },
+  {
+    "revision": "f7161f8be9b82ed7ca8f",
+    "url": "/vue-pwa-test/js/chunk-vendors.4dcbfa82.js"
+  },
+  {
+    "revision": "3f40beb92ac954e017d428dee829c1bd",
+    "url": "/vue-pwa-test/img/timg.3f40beb9.jpg"
+  },
+  {
+    "revision": "70893f75b41558fd3db1137880a89175",
+    "url": "/vue-pwa-test/index.html"
+  },
+  {
+    "revision": "3f40beb92ac954e017d428dee829c1bd",
+    "url": "/vue-pwa-test/timg.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-pwa-test/robots.txt"
+  }
+];
